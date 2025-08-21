15f7f169811b44a11d6e748980e8e5340a9431f2
carlosegoulart <goulart193@gmail.com>
2025-08-21 17:39:01 +0000

    Upd Mobile_2025.2

---
+++ my-app/components/Login.tsx
@@ -0,0 +1,41 @@
+import { StyleSheet, View, Text, TextInput, Button } from 'react-native'
+import React from 'react'
+
+export default function Login() {
+  const [username, onChangeUser] = React.useState('Seu login');
+  const [password, onChangePass] = React.useState('Sua senha');
+
+  const handleLogin = () => {
+    console.log("Usuario: " + username);
+    console.log("Senha: " + password);
+  }
+
+  return (
+    <View>
+        <Text>User</Text>
+        <TextInput
+            style={styles.input}
+            onChangeText={onChangeUser}
+            value={username}
+        />
+
+        <Text>Password</Text>
+        <TextInput
+            style={styles.input}
+            onChangeText={onChangePass}
+            value={password}
+            secureTextEntry={true}
+        />
+        <Button title='Login' onPress={handleLogin}/>
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({
+    input: {
+      height: 40,
+      margin: 12,
+      borderWidth: 1,
+      padding: 10,
+    },
+  });
